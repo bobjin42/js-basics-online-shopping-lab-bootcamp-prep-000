cbc872b2bbf3d257e586924ce289f5dae44e6e96
Yicheng Jin <bobjin42@gmail.com>
2018-05-21 20:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber === undefined){
-    return `
+    return `Sorry, we don't have a credit card on file for you.`
+  }else{
+    cart = [];
   }
 }
