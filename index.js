6fc499a9cd7ab6bf8fb413478457ee742db8ad31
Yicheng Jin <bobjin42@gmail.com>
2018-05-21 20:30:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  if(cart.length === 0){
+    return `Your shopping cart is empty.`
+  }
   var firstEle = `${cart[0].itemName} at $${cart[0].itemPrice}`;
   var lastEle = `${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}`
   var newStr = "";
-  if(cart.length === 0){
-    return `Your shopping cart is empty.`
-  }else if(cart.length === 1){
+else if(cart.length === 1){
     return `In your cart, you have ${firstEle}.`
   }
   for(var i = 1; i < cart.length -1; i++){
